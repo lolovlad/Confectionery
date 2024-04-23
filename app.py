f8ea9288b8ead421c1678865b878bac952430c06
lolovlad <lolovlad33@gmail.com>
2024-04-23 16:12:31 +0400

    docker

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ admin.add_view(BakeryAdminView(Bakery, db.session, name="Пекарня"))
 admin.add_link(LogoutMenuLink(name="Выход", category="", url="/logout"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
